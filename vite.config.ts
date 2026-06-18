fc45a613fd17bc8879c8c064e6c810ca04165150
キラメイ <1710623513@qq.com>
2026-06-18 23:26:18 +0800

    Fix: Do not expose to outside in tauri mode

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig(({ mode }) => {
       __WITH_TAURI__: mode === "tauri",
     },
     server: {
-      host: "0.0.0.0",
+      host: mode === "tauri" ? "127.0.0.1" : "0.0.0.0",
       port: mode === "tauri" ? 8191 : 8192,
       strictPort: true,
     },
